8b61c67cc41b10476add27ec218cdfb9fcce2c45
Richard Suthers <rsuthers@LUK-5CD54998HQ.localdomain>
2026-05-13 17:03:00 +0100

    Fixed issue for No module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,19 +1,19 @@
 resource "oci_core_vcn" "demo_vcn" {
-  compartment_id = module.compartments.network_compartment_id
+  compartment_id = var.compartment_ocid
   cidr_block     = "10.0.0.0/16"
   display_name   = "demo-test-vcn"
   dns_label      = "demotestvcn"
 }
 
 resource "oci_core_internet_gateway" "demo_igw" {
-  compartment_id = module.compartments.network_compartment_id
+  compartment_id = var.compartment_ocid
   vcn_id         = oci_core_vcn.demo_vcn.id
   display_name   = "demo-test-igw"
   enabled        = true
 }
 
 resource "oci_core_route_table" "demo_rt" {
-  compartment_id = module.compartments.network_compartment_id
+  compartment_id = var.compartment_ocid
   vcn_id         = oci_core_vcn.demo_vcn.id
   display_name   = "demo-test-rt"
 
@@ -25,7 +25,7 @@ resource "oci_core_route_table" "demo_rt" {
 }
 
 resource "oci_core_security_list" "demo_sl" {
-  compartment_id = module.compartments.network_compartment_id
+  compartment_id = var.compartment_ocid
   vcn_id         = oci_core_vcn.demo_vcn.id
   display_name   = "demo-test-sl"
 
@@ -46,7 +46,7 @@ resource "oci_core_security_list" "demo_sl" {
 }
 
 resource "oci_core_subnet" "demo_public_subnet" {
-  compartment_id             = module.compartments.network_compartment_id
+  compartment_id             = var.compartment_ocid
   vcn_id                     = oci_core_vcn.demo_vcn.id
   cidr_block                 = "10.0.1.0/24"
   display_name               = "demo-public-subnet"
